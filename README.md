efe887e3da532bbed06fde30fff4da411cad7de8
Lucas Mattos Amorim <amorim22lucas@gmail.com>
2025-12-04 15:11:43 -0300

    make final adjust

---
+++ README.md
@@ -0,0 +1,14 @@
+Desafio Projeto Orientação de Objetos com Java
+
+Este projeto buscou explicar e aplicar os conceitos de orientação utilizando a linguagem Java
+
+Conceitos abordados:
+
+- Abstração
+- Encapsulamento
+- Herança
+- Polimorfismo
+
+<img src="assets/result.png"></img>
+
+Print do resultado final do exercicio
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
+import br.com.dio.desafio.dominio.Bootcamp;
 import br.com.dio.desafio.dominio.Conteudo;
 import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
 import br.com.dio.desafio.dominio.Mentoria;
 
 import java.time.LocalDate;
@@ -22,10 +24,37 @@ public class Main {
         mentoria.setDescricao("Descrição Mentoria de java");
         mentoria.setData(LocalDate.now());
 
-        // Test
-        System.out.println(curso1);
-        System.out.println(curso2);
-        System.out.println(mentoria);
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp JAVA Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devCamila = new Dev();
+        devCamila.setNome("Camila");
+        devCamila.inscreverBootcamp(bootcamp);
+        System.out.println("Conteudos Incritos de Camila: " + devCamila.getConteudosInscritos());
+        devCamila.progredir();
+        System.out.println("Camila Progredindo...");
+
+        System.out.println("Conteudos Incritos de Camila: " + devCamila.getConteudosInscritos());
+        System.out.println("Conteudos Concluidos de Camila: " + devCamila.getConteudosConcluidos());
+        System.out.println("XP de Camila: "+devCamila.calcularTotalXp());
+
+        System.out.println("---------------------------------------------");
+
+        Dev devJoao = new Dev();
+        devJoao.setNome("João");
+        devJoao.inscreverBootcamp(bootcamp);
+        System.out.println("Conteudos Incritos de João: " + devJoao.getConteudosInscritos());
+        devJoao.progredir();
+        devJoao.progredir();
+        System.out.println("João Progredindo...");
+        System.out.println("Conteudos Incritos de João: " + devJoao.getConteudosInscritos());
+        System.out.println("Conteudos Concluidos de João: " + devJoao.getConteudosConcluidos());
+        System.out.println("XP de João: "+devJoao.calcularTotalXp());
     }
 
 }
